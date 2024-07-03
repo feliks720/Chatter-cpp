c263d86a135764f4e46270c47261935d648229c5
fazhenghan <hanfazheng@gmail.com>
2024-07-03 11:14:56 -0600

    Day 1 with extra files.

--- client/llfcchat/main.cpp
+++ client/llfcchat/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QFile qss("stylesheet.qss");
+    QFile qss(":/style/stylesheet.qss");
 
     if( qss.open(QFile::ReadOnly))
     {
