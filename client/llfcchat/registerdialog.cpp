e9b03040d76db351d8b560dd609136b0fd5fdaa1
fazhenghan <hanfazheng@gmail.com>
2024-07-08 13:57:31 -0700

    Day2 client network class design and implement

--- client/llfcchat/registerdialog.cpp
+++ client/llfcchat/registerdialog.cpp
@@ -6,6 +6,10 @@ RegisterDialog::RegisterDialog(QWidget *parent) :
     ui(new Ui::RegisterDialog)
 {
     ui->setupUi(this);
+    //设置密码格式隐藏
+    ui->pass_edit->setEchoMode(QLineEdit::Password);
+    ui->confirm_edit->setEchoMode(QLineEdit::Password);
+    ui->err_tip->setProperty("state","normal");
 }
 
 RegisterDialog::~RegisterDialog()
